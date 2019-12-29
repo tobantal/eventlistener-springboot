c1965ff885a56ad76bd4fdb6887e801c7d638d58
Anton Tobolkin <tobantal@gmail.com>
2019-12-29 08:35:34 +0000

    first commit

---
+++ src/main/java/org/rpis5/chapters/ChaptersApplication.java
@@ -0,0 +1,13 @@
+package org.rpis5.chapters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaptersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaptersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/rpis5/chapters/ChaptersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rpis5.chapters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaptersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
